b72c30e838d277bc3ec000ee7f3367fef105534e
Frankyston Lins <frankyston@gmail.com>
2019-12-31 21:53:36 -0300

    Adds the user create endpoint

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,4 +3,19 @@ class Api::V1::UsersController < ApplicationController
     @user = User.find(params[:id])
     render json: @user, status: :success
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user, status: :created
+    else
+      render json: { errors: @user.errors }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
 end
--- test/controllers/api/v1/users_controller_test.rb
+++ test/controllers/api/v1/users_controller_test.rb
@@ -10,4 +10,27 @@ class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
     response_json = JSON.parse(response.body)
     assert_equal @user.email, response_json['email']
   end
+
+  test 'should create user' do
+    post api_v1_users_url, params: {
+      user: {
+        email: 'test@teste.com',
+        password: 'super123'
+      }
+    }, as: :json
+
+    response_json = JSON.parse(response.body)
+    assert_response :created
+  end
+
+  test 'should not create user with taken email' do
+    post api_v1_users_url, params: {
+      user: {
+        email: @user.email,
+        password: 'super123'
+      }
+    }, as: :json
+
+    assert_response :unprocessable_entity
+  end
 end
