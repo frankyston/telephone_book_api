2181293eec81bc8f59a9320753c7e785690b8b89
Frankyston Lins <frankyston@gmail.com>
2020-01-01 18:20:28 -0300

    Adjusts relationship contacts in User

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
   validates :email, uniqueness: true
   validates_format_of :email, with: /@/ 
   validates :password_digest, presence: true
-  has_many :contact, dependent: :destroy
+  has_many :contacts, dependent: :destroy
 end
